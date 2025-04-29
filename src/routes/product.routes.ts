18c0cd99c77c550c8054691d29e329b20769e2ad
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-04-29 13:39:51 +0530

    product route request validations

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import ProductRepository from '../repository/product.repository';
 import ProductService from '../service/product.service';
 import ProductController from '../controller/product.controller';
+import { ProductSchema } from '../utils/validations';
+import { celebrate } from 'celebrate';
 
 const productRouter = express.Router();
 
@@ -10,9 +12,25 @@ const productService = new ProductService(productRepository);
 const productController = new ProductController(productService);
 
 productRouter.get('/', productController.getProducts);
-productRouter.get('/:id', productController.getProductById);
-productRouter.post('/', productController.createProduct);
-productRouter.put('/:id', productController.updateProduct);
-productRouter.delete('/:id', productController.deleteProduct);
+productRouter.get(
+  '/:id',
+  celebrate(ProductSchema.get),
+  productController.getProductById
+);
+productRouter.post(
+  '/',
+  celebrate(ProductSchema.create),
+  productController.createProduct
+);
+productRouter.put(
+  '/:id',
+  celebrate(ProductSchema.update),
+  productController.updateProduct
+);
+productRouter.delete(
+  '/:id',
+  celebrate(ProductSchema.delete),
+  productController.deleteProduct
+);
 
 export default productRouter;
--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -25,3 +25,37 @@ export const categorySchema = {
     }
   }
 };
+
+export const ProductSchema = {
+  create: {
+    body: {
+      name: Joi.string().required(),
+      quantity: Joi.number().required(),
+      price: Joi.number().required(),
+      description: Joi.string().required(),
+      category_id: Joi.number().required()
+    }
+  },
+  update: {
+    body: {
+      name: Joi.string().required(),
+      quantity: Joi.number().required(),
+      price: Joi.number().required(),
+      description: Joi.string().required(),
+      category_id: Joi.number().required()
+    },
+    params: {
+      id: Joi.number().required()
+    }
+  },
+  delete: {
+    params: {
+      id: Joi.number().required()
+    }
+  },
+  get: {
+    params: {
+      id: Joi.number().required()
+    }
+  }
+};
