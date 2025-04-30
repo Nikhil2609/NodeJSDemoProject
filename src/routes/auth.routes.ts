db9d33a3d6e08aa078c9d7b9c43f9cdccfa2e23d
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-04-30 17:06:48 +0530

    implement request limiter for api for to handle brute force attacks

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import AuthRepository from '../repository/auth.repository';
 import AuthService from '../service/auth.service';
 import AuthController from '../controller/auth.controller';
+import rateLimiter from "express-rate-limit";
 
 const authRouter = express.Router();
 
@@ -9,7 +10,14 @@ const authRepository = new AuthRepository();
 const authService = new AuthService(authRepository);
 const authController = new AuthController(authService);
 
-authRouter.post('/login', authController.login);
+
+const loginLimiter = rateLimiter({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 5, // limit each IP to 5 request
+    message: { error: "Too many login attempts from this IP, please try again later" }
+});
+
+authRouter.post('/login', loginLimiter, authController.login);
 authRouter.post('/register', authController.register);
 
 export default authRouter;
